5c78872b4a1ac2d6bbede64cc8bfe520fa8c2b03
SAPIENT\vsha95 <vsharma109@sapient.com>
2017-11-30 12:31:16 +0530

    create basic component for angular2

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  // stayle is priority 
+  // styles: [`
+  //   h3 {
+  //     color:blue;
+  //   }
+  // `]
+})
+export class AppComponent {
+  
+}
---
+++ src/app/successalert/successalert.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-successalert',
+    templateUrl: './successalert.component.html',
+    styleUrls: ['./successalert.component.css']
+})
+
+export class SuccessAlertComponent {}
